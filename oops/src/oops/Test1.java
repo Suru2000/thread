885e63c7993156da4527bdfde70d4661adb7de55
Suru2000 <86404115+Suru2000@users.noreply.github.com>
2023-05-12 18:23:55 +0530

    oops.java

---
+++ oops/src/oops/Test1.java
@@ -0,0 +1,9 @@
+package oops;
+public class Test1
+{ 
+    public static void main(String args[])  
+    { 
+        MountainBike mb = new MountainBike(3, 100, 25); 
+        System.out.println(mb.toString());
+    } 
+}
